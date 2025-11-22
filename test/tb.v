d289750c9d1b919f9296a523b76ce9f586f40c76
rej <rzioma@acm.org>
2025-11-22 14:07:36 +0100

    Updated to ihp130 workflow.

--- test/tb.v
+++ test/tb.v
@@ -6,7 +6,7 @@
 */
 module tb ();
 
-  // Dump the signals to a VCD file. You can view it with gtkwave.
+  // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
   initial begin
     $dumpfile("tb.vcd");
     $dumpvars(0, tb);
@@ -25,12 +25,6 @@ module tb ();
 
   // Replace tt_um_example with your module name:
   tt_um_rejunity_vga_test01 user_project (
-
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(1'b1),
-      .VGND(1'b0),
-`endif
 
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
